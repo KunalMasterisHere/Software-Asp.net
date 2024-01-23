d5c7a261ef928425b0c0272318dfc6e4a6e25bd5
KunalMasterisHere <90489504+KunalMasterisHere@users.noreply.github.com>
2024-01-23 21:34:22 +0530

    Add files via upload

---
+++ ASPDOTNET/File Upload.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true" CodeFile="File Upload.aspx.cs" Inherits="File_Upload" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <div align="center">
+        <div>
+            <asp:FileUpload ID="FileUpload1" runat="server" />  
+        </div>
+        <div>
+            <asp:Button ID="Button1" runat="server" Text="Button" onclick="Button1_Click" />
+        </div>
+        <div>
+            <asp:Literal ID="Literal1" runat="server"></asp:Literal>
+        </div>
+    </div>
+</asp:Content>
+
---
+++ ASPDOTNET/File Upload.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class File_Upload : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        if (FileUpload1.HasFile == null || FileUpload1.PostedFile.ContentLength > 0)
+        {
+            string path = System.IO.Path.GetExtension(FileUpload1.FileName);
+            if (path == ".png" || path == ".jpg" || path == ".jpeg" || path == ".jfif")
+            {
+                string location = Server.MapPath("files") + "\\" + FileUpload1.FileName;
+                FileUpload1.SaveAs(location);
+                Literal1.Text = "File Name: " + FileUpload1.FileName;
+                Literal1.Text += "<br>" + "File Type: " + FileUpload1.PostedFile.ContentType;
+                Literal1.Text += "<br>" + "File Size:" + FileUpload1.PostedFile.ContentLength;
+
+            }
+            else
+            {
+                Literal1.Text = "Please Select Image File Only!";
+            }
+        }
+        else
+        {
+            Literal1.Text = "Please Select Any File First!";
+        }
+    }
+}
